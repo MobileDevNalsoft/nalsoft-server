822fc9303bb45a794a0810d51cf5965328bfbb0d
saisanjit <saisanjit.d@gmail.com>
2024-03-08 15:31:24 +0530

    Updated

--- index.js
+++ index.js
@@ -1,80 +1,71 @@
-// import { initializeApp, applicationDefault } from 'firebase-admin/app';
-// import { getMessaging } from "firebase-admin/messaging";
-// import express, { json } from "express";
-// import cors from "cors";
 var admin = require("firebase-admin");
 var serviceAccount = require("./service.json");
 const google = require('googleapis');
 const keys = require('./service.json');
 const cors = require('cors');
-const express =require('express');
+const express = require('express');
 
 process.env.GOOGLE_APPLICATION_CREDENTIALS;
-
-
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
+
 const app = express();
 app.use(express.json());
 
 app.use(
-    cors({
-        origin: "*",
-    })
+  cors({
+    origin: "*",
+  })
 );
 
 app.use(
-    cors({
-        methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
-    })
+  cors({
+    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
+  })
 );
 
 app.use(function (req, res, next) {
-    res.setHeader("Content-Type", "application/json");
-    next();
+  res.setHeader("Content-Type", "application/json");
+  next();
 });
 
-
-
-
 app.get("/", function (req, res) {
-    var token;
-    new Promise(function(resolve, reject) {
-      const key = require('./service.json');
-      const jwtClient = new google.Auth.JWT(
-        key.client_email,
-        null,
-        key.private_key,
-        ["https://www.googleapis.com/auth/firebase.messaging"],
-        null
-      );
-      jwtClient.authorize(function(err, tokens) {
-        if (err) {
-          reject(err);
-          return;
-        }
-        resolve(tokens.access_token);
-        token=tokens.access_token
-        console.log(tokens.access_token);
-        // return tokens.access_token;
-      });
-    }).then((response) => {
-        res.status(200).json({
-            access_token: token,
-        });
-        console.log("Successfully sent :", response);
-    })
-    .catch((error) => {
-        res.status(400);
-        res.send(error);
-        console.log("Error sending :", error);
+  var token;
+  new Promise(function (resolve, reject) {
+    const key = require('./service.json');
+    const jwtClient = new google.Auth.JWT(
+      key.client_email,
+      null,
+      key.private_key,
+      ["https://www.googleapis.com/auth/firebase.messaging"],
+      null
+    );
+    jwtClient.authorize(function (err, tokens) {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(tokens.access_token);
+      token = tokens.access_token
+      console.log(tokens.access_token);
     });
-  
+  }).then((response) => {
+    res.status(200).json({
+      access_token: token,
+    });
+    console.log("Successfully sent :", response);
+  })
+    .catch((error) => {
+      res.status(400);
+      res.send(error);
+      console.log("Error sending :", error);
+    });
+
 });
 
 
 app.listen(3000, function () {
-    console.log("Server started on port 3000");
+  console.log("Server started on port 3000");
 });
